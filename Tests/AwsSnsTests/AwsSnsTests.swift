9f4c805ff662227816069f7292440d8c8efe5fe9
Nikola Mladenovic <nikola@mladenovic.biz>
2017-09-18 12:25:07 +0200

    Updated tests

--- Tests/AwsSnsTests/AwsSnsTests.swift
+++ Tests/AwsSnsTests/AwsSnsTests.swift
@@ -86,10 +86,6 @@ class AwsSnsTests: XCTestCase {
         let token = "6110cff81aea420ef5017029ef82dd5b44a018fae1c9a83a6ca8be5b4109bceb"
         
         snsClient?.createPlatformEndpoint(token: token, platformApplicationArn: "arn:aws:sns:us-west-2:487164526243:app/APNS_SANDBOX/Test") { success, endpointArn, error in
-            guard let endpointArn = endpointArn else {
-                XCTFail("CreatePlatformEndpoint returned nil.")
-                return
-            }
             self.snsClient?.deleteEndpoint(endpointArn: endpointArn) { success, error in
                 XCTAssertNil(error, "DeletePlatformEndpoint returned error.")
                 deleteExpectation.fulfill()
