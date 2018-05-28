d8489fea05e80eb05b28525283426b41d3808b9f
Nikola Mladenovic <nikola@mladenovic.biz>
2018-05-29 01:25:26 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "AwsSns",
     products: [.library(name: "AwsSns", targets: ["AwsSns"])],
-    dependencies: [.package(url: "https://github.com/nikola-mladenovic/AwsSwiftSign.git", .branch("master")),
+    dependencies: [.package(url: "https://github.com/nikola-mladenovic/AwsSwiftSign.git", from: "0.1.0"),
                    .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "4.1.1")],
     targets: [.target(name: "AwsSns", dependencies: ["AwsSign", "SWXMLHash"]),
               .testTarget(name: "AwsSnsTests", dependencies: ["AwsSns"])]
