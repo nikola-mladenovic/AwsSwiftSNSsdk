e5808d2f788e6d2459319bdefe15be24951c0d1f
Nikola Mladenovic <nikola@mladenovic.biz>
2018-10-08 01:55:26 +0300

    Swift 4.2 support

--- Package.swift
+++ Package.swift
@@ -1,12 +1,13 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
     name: "AwsSns",
     products: [.library(name: "AwsSns", targets: ["AwsSns"])],
-    dependencies: [.package(url: "https://github.com/nikola-mladenovic/AwsSwiftSign.git", from: "0.1.0"),
-                   .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "4.1.1")],
+    dependencies: [.package(url: "https://github.com/nikola-mladenovic/AwsSwiftSign.git", from: "0.2.0"),
+                   .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "4.7.1")],
     targets: [.target(name: "AwsSns", dependencies: ["AwsSign", "SWXMLHash"]),
-              .testTarget(name: "AwsSnsTests", dependencies: ["AwsSns"])]
+              .testTarget(name: "AwsSnsTests", dependencies: ["AwsSns"])],
+    swiftLanguageVersions: [.v4_2]
 )
