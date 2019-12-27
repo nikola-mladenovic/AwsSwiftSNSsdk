648e57e89f08b99bfefa799daa05b42e22d2a0dc
Nikola Mladenovic <nikola@mladenovic.biz>
2019-12-27 19:33:26 +0100

    Swift 5.1 support

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "AwsSns",
     products: [.library(name: "AwsSns", targets: ["AwsSns"])],
-    dependencies: [.package(url: "https://github.com/nikola-mladenovic/AwsSwiftSign.git", from: "0.2.0"),
-                   .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "4.7.1")],
+    dependencies: [.package(url: "https://github.com/nikola-mladenovic/AwsSwiftSign.git", from: "0.3.0"),
+                   .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "5.0.1")],
     targets: [.target(name: "AwsSns", dependencies: ["AwsSign", "SWXMLHash"]),
               .testTarget(name: "AwsSnsTests", dependencies: ["AwsSns"])],
-    swiftLanguageVersions: [.v4_2]
+    swiftLanguageVersions: [.v5]
 )
--- Sources/AwsSns/AwsSns.swift
+++ Sources/AwsSns/AwsSns.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 import AwsSign
 import SWXMLHash
 
